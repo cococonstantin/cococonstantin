1cfd5e0e4bb5aa8a1e8bdb8c86514ec20ba37eb8
Corinne Beaudoin-Pellerin <corinne.p.beaudoin@gmail.com>
2020-03-30 15:51:34 -0400

    Initial Commit

---
+++ ass 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ass 4
+//
+//  Created by Corinne Beaudoin-Pellerin on 2020-03-30.
+//  Copyright © 2020 Corinne Beaudoin-Pellerin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
